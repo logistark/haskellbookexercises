94368eee4765b40ca875292fbc6d00de75d9f51d
Jose Antonio Jimenez Saez <jimenezsaezjoseantonio@gmail.com>
2016-08-19 18:39:22 +0200

    Fixed arith function

--- 6 - Typeclasses/typekwondo2.hs
+++ 6 - Typeclasses/typekwondo2.hs
@@ -3,8 +3,5 @@ module Typekwondo2 where
   chk :: Eq b => (a -> b) -> a -> b -> Bool
   chk f x y = (f x) == y
 
-  --arith :: Num b => (a -> b) -> Integer -> a -> b
-  --arith f x y = x
-
-  test :: Num a => Integer -> a
-  test v = v
+  arith :: Num b => (a -> b) -> Integer -> a -> b
+  arith f x y = (f y) + fromInteger x
